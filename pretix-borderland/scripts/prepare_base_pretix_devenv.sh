0d16af66471cd7eea94861f2ed70a629a43b073f
Arturo Escudero <darkturo@gmail.com>
2022-05-01 23:45:25 +0200

    Fix script to run pipinstall on fresh

--- pretix-borderland/scripts/prepare_base_pretix_devenv.sh
+++ pretix-borderland/scripts/prepare_base_pretix_devenv.sh
@@ -6,5 +6,14 @@ echo Building local development environment based on Pretix $PRETIX_TAG
 mkdir -p build 
 git clone --branch v4.7.1 --single-branch --depth 1 https://github.com/pretix/pretix.git build/pretix
 
-pushd build/pretix/src
-pipenv --python 3.9 run pip3 install -e . 
+(
+	BASE_DIR=$(pwd)
+	cd build/pretix/src
+
+	pipenv --rm
+	pipenv --python 3.9 install -e .
+
+	ln -s $BASE_DIR/pretix_borderland/pretix-locale/en_BL pretix/locale
+	ln -s $BASE_DIR/pretix_borderland pretix/plugins
+
+)
