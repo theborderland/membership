f3daca769c1094b42be13a29280c08d33af54a40
aleksd-dfds <aldub@dfds.com>
2022-05-15 10:08:04 +0200

    test fetch and raffle

--- borderland-tool/borderland_tool/test_parsers.py
+++ borderland-tool/borderland_tool/test_parsers.py
@@ -8,7 +8,29 @@ class TestParsers(unittest.TestCase):
     lotterycmd = LotteryCmd()
 
     def setUp(self):
-        pass
+        self.parser = argparse.ArgumentParser(
+            description='A collection of things we do with Pretix')
+        subparsers = self.parser.add_subparsers(dest='cmd')
+        subparsers.required = True
+
+        # Common Arguments
+        self.parser.add_argument("-t", "--token",
+                                 required=True,
+                                 help="api token for pretix (required)")
+        self.parser.add_argument("-s", "--server", metavar="HOST",
+                                 default="localhost:8000",
+                                 help="hostname of pretix instance")
+        self.parser.add_argument("-o", "--org",
+                                 default="test",
+                                 help="pretix organisation to operate on")
+        self.parser.add_argument("-e", "--event",
+                                 default="2022", help="pretix event to operate on")
+        self.parser.add_argument("-N", "--no-ssl", action='store_true',
+                                 help="disable ssl when communicating with pretix, good for testing locally")
+
+        # Lottery
+        lotterycmd = LotteryCmd()
+        lotterycmd.add_parser(subparsers)
 
     def test_argparse(self):
         simple_parser = argparse.ArgumentParser(
@@ -30,35 +52,28 @@ class TestParsers(unittest.TestCase):
         self.assertEqual(result_args.arg1, "ASDF")
         self.assertEqual(result_args.arg2, "QWER")
 
-    def test_lottery_args(self):
-        parser = argparse.ArgumentParser(
-            description='A collection of things we do with Pretix')
-        subparsers = parser.add_subparsers(dest='cmd')
-        subparsers.required = True
-
-        # Common Arguments
-        parser.add_argument("-t", "--token",
-                            required=True,
-                            help="api token for pretix (required)")
-        parser.add_argument("-s", "--server", metavar="HOST",
-                            default="localhost:8000",
-                            help="hostname of pretix instance")
-        parser.add_argument("-o", "--org",
-                            default="test",
-                            help="pretix organisation to operate on")
-        parser.add_argument("-e", "--event",
-                            default="2022", help="pretix event to operate on")
-        parser.add_argument("-N", "--no-ssl", action='store_true',
-                            help="disable ssl when communicating with pretix, good for testing locally")
-
-        # Lottery
-        lotterycmd = LotteryCmd()
-        lotterycmd.add_parser(subparsers)
+    def test_lottery_args_fetch(self):
         cmd_args = ['-t', 'mytoken', '-s', 'myserver',
-                    '-o', 'myorg', '-e', 'myevent', 'lottery', '-q', '88', 'fetch']
-        args = parser.parse_args(cmd_args)
+                    '-o', 'myorg', '-e', 'myevent',
+                    'lottery', '-q', '88', 'fetch']
+        args = self.parser.parse_args(cmd_args)
         self.assertEqual(args.cmd, 'lottery')
         self.assertEqual(args.lottery_action, 'fetch')
+        self.assertEqual(args.quota, 88)
+        pretix = get_pretix(args)
+        self.assertIsNotNone(pretix)
+        self.assertEqual(pretix.org, "myorg")
+        self.assertEqual(pretix.host, "myserver")
+        self.assertEqual(pretix.event, "myevent")
+        self.assertEqual(pretix.token, "mytoken")
+
+    def test_lottery_args_raffle(self):
+        cmd_args = ['-t', 'mytoken', '-s', 'myserver',
+                    '-o', 'myorg', '-e', 'myevent',
+                    'lottery', '-q', '88', 'raffle', '-n', '10']
+        args = self.parser.parse_args(cmd_args)
+        self.assertEqual(args.cmd, 'lottery')
+        self.assertEqual(args.lottery_action, 'raffle')
         self.assertEqual(args.quota, 88)
         pretix = get_pretix(args)
         self.assertIsNotNone(pretix)
