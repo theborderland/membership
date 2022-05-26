cff403a36756d44ae0658dadead3d68b5d56ac28
Arturo Escudero <darkturo@gmail.com>
2022-05-27 01:12:16 +0200

    add a model to support waiting lists

--- pretix-borderland/pretix_borderland/models/__init__.py
+++ pretix-borderland/pretix_borderland/models/__init__.py
@@ -1,2 +1,3 @@
 from .lottery_entry import LotteryEntry
 from .refund import RefundRequest
+from .waiting_list import WaitingList
--- pretix-borderland/pretix_borderland/models/waiting_list.py
+++ pretix-borderland/pretix_borderland/models/waiting_list.py
@@ -4,26 +4,20 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 
-class LotteryEntry(LoggedModel):
+class WaitingList(LoggedModel):
     event = models.ForeignKey('pretixbase.Event', on_delete=models.CASCADE)
     email = models.EmailField(verbose_name="E-mail address")
     first_name = models.CharField(max_length=200, verbose_name="Legal First Name")
     last_name = models.CharField(max_length=200, verbose_name="Legal Last Name")
     dob = models.DateField(verbose_name="Date of Birth")
     timestamp = models.DateTimeField(auto_now_add=True)
-    vouchers = models.ManyToManyField('pretixbase.Voucher')
-    ip = models.CharField(max_length=200, blank=True)
-    cookie = models.CharField(max_length=2048, blank=True)
-    browser = models.CharField(max_length=2048, blank=True)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=["event", "email"], name="register_email_once"),
-            #            models.UniqueConstraint(fields=["event", "first_name", "last_name", "dob"],
-            #                                    name="register_person_once")
         ]
 
 
-@receiver(pre_save, sender=LotteryEntry)
+@receiver(pre_save, sender=WaitingList)
 def pre_save(sender, instance, **kwargs):
     instance.email = instance.email.lower()
