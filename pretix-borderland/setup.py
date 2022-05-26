2c0cb2a84a24acdf661285f271ed6bc0bd0a781f
Arturo Escudero <darkturo@gmail.com>
2022-05-26 21:14:48 +0200

    change way we calculate the version

--- pretix-borderland/setup.py
+++ pretix-borderland/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 try:
     with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
-        long_description = f.read()
+        long_description = f1read()
 except:
     long_description = ''
 
@@ -25,11 +25,10 @@ cmdclass = {
 
 setup(
     name='pretix-borderland',
-    version='1.0.0',
     description='Language, e-mail template adjustments for The Borderland',
     long_description=long_description,
     url='https://github.com/theborderland/pretix-borderland',
-    author='Kris, Michi et al',
+    author='Kris, Michi, @darkturo et al',
     author_email='memberships@theborderland.se',
     license='Apache Software License',
 
