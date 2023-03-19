1b7cc701d14192043411fdd54bbacfdcb0708257
Arturo Escudero <darkturo@gmail.com>
2023-03-19 18:24:16 +0100

    Fix issue with deprecated module (latest django)

--- pretix-borderland/scripts/prepare_base_pretix_devenv.sh
+++ pretix-borderland/scripts/prepare_base_pretix_devenv.sh
@@ -11,7 +11,9 @@ git clone --branch v4.7.1 --single-branch --depth 1 https://github.com/pretix/pr
 	cd build/pretix/src
 
 	pipenv --rm
+	pipenv install "django-mysql==4.5.0"
 	pipenv --python 3.9 install -e .
+	pipenv install "django-mysql==4.5.0"
 
 	ln -s $BASE_DIR/pretix_borderland/pretix-locale/en_BL pretix/locale
 	ln -s $BASE_DIR/pretix_borderland pretix/plugins
