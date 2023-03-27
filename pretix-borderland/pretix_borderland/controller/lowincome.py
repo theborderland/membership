929763f5862bbc67f72927a82592b71e649f69f6
Arturo Escudero <darkturo@gmail.com>
2023-03-27 18:43:10 +0200

    update algorithm

--- pretix-borderland/pretix_borderland/controller/lowincome.py
+++ pretix-borderland/pretix_borderland/controller/lowincome.py
@@ -2,7 +2,13 @@ from ..models import LowIncomeEntry, YesNo, YesNoDontKnowPreferNotToSay
 
 
 def IsEligibleForLowIncome(entry: LowIncomeEntry):
-    points = 0
+    minimum_wage_euro = 1000.0
+    income = 0.0
 
+    if entry.has_assets == YesNo.Yes:
+        return False
 
-    return points <= 10
+    if entry.has_income == YesNo.Yes:
+        income += entry.income
+
+    return income <= minimum_wage_euro
