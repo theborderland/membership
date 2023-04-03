125321ae801e9dd80a14a4e9a17d644d9782ad62
Arturo Escudero <darkturo@gmail.com>
2023-04-03 03:17:14 +0200

    add a simple script to filter out email addresses that for the same person
    To identify a person I used the first name, last name,  and date of
    birth and the cookie
    
    I have decided not to punish the cheaters, just to normalise their
    registration.

---
+++ borderland-tool/hacks/cheaters.py
@@ -0,0 +1,19 @@
+import csv
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 cheaters.py <csv file>")
+        sys.exit(1)
+
+    with open(sys.argv[1], newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        seen = set()
+        for row in reader:
+            key = (row['first_name'], row['last_name'], row['dob'], row['cookie'])
+            if key not in seen:
+                seen.add(key)
+                print(row)
+
+if __name__ == '__main__':
+    main()
