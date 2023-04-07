9641df04915b14fee3fcd3813d5390fee5a06026
Arturo Escudero <darkturo@gmail.com>
2023-04-07 16:47:44 +0200

    update hacks to order the headers in the intender order ...

--- borderland-tool/hacks/filter_cheaters.py
+++ borderland-tool/hacks/filter_cheaters.py
@@ -9,12 +9,12 @@ def main():
     with open(sys.argv[1], newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         seen = set()
-        print("id,email,first_name,last_name,dob,timestamp,ip,cookie,browser,low_income")
+        print("id,email,first_name,last_name,dob,low_income,timestamp,ip,cookie,browser")
         for row in reader:
             key = (row['first_name'], row['last_name'], row['dob'], row['cookie'])
             if key not in seen:
                 seen.add(key)
-                print(f"{row['id']},{row['email']},{row['first_name']},{row['last_name']},{row['dob']},{row['timestamp']},{row['ip']},{row['cookie']},{row['browser']},{row['low_income']}".format(row))
+                print(f"{row['id']},{row['email']},{row['first_name']},{row['last_name']},{row['dob']},{row['low_income']},{row['timestamp']},{row['ip']},{row['cookie']},{row['browser']}".format(row))
 
 
 if __name__ == '__main__':
