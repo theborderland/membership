e4a8fd69215f701d85dc045e17e010967e51c414
Arturo Escudero <darkturo@gmail.com>
2024-02-17 14:29:47 +0100

    Delete prepare script, rely fully on the makefile

--- pretix-borderland/scripts/prepare_base_pretix_devenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#PRETIX_TAG=v4.7.1
-PRETIX_TAG=v2024.1.0
-
-pipenv --rm
-pipenv --python 3.11.2 run pip3 install -U "git+https://github.com/pretix/pretix.git@$PRETIX_TAG#egg=pretix"
-pipenv run python -m pretix migrate
-pipenv run python -m pretix rebuild
-pipenv run python -m pretix updatestyles
